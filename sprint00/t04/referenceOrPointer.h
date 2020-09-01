fb93c394582736d9ba4f57937ffe8c7a0dd9b85b
michael-boyko <kozak.miksa@gmail.com>
2020-09-01 22:50:54 +0300

    t05 in work

---
+++ sprint00/t04/referenceOrPointer.h
@@ -0,0 +1,9 @@
+#ifndef REFERENCEORPOINTER_H
+#define REFERENCEORPOINTER_H 
+
+#include <iostream>
+
+void multiplyByPointer(int* ptr, int mult);
+void multiplyByReference(int& ref, int mult);
+
+#endif
