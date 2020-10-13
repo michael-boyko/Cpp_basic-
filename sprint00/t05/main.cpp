075a831c53c4864df5a0320a37a449eea8b32ef5
michael-boyko <kozak.miksa@gmail.com>
2020-10-13 12:08:32 +0300

    t02 done

---
+++ sprint00/t05/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+    if (argc > 4 || argc < 2) {
+
+    }
+    auto Name = 
+    return 0;
+}
--- sprint01/t02/main.cpp
+++ sprint01/t02/main.cpp
@@ -12,55 +12,105 @@ typedef struct s_town {
     int id;
 } t_town;
 
-void check(std::deque<t_town *> d_towns, char *argv[]) {
+int count_words(const std::string &str) {
+    int count = 0;
 
+    for (long unsigned int i = 0; i < str.size(); i++) {
+        if (str[i] == ',') {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+bool check_input(std::deque<t_town *> &towns, const std::vector<std::string> &arr) {
+    int from = 0;
+    int to = 0;
+    int id = 0;
+    std::string tmp;
+    t_town *town = NULL;
+
+
+    for (auto const &element : arr) {
+        if (count_words(element) != 2) {
+            std::cout << "Argument " << element << " is not valid" << std::endl;
+            return false;
+        } else {
+            town = new t_town;
+            to = element.find(",");
+            tmp = element.substr(0, to);
+            if (tmp.empty()) {
+                std::cout << "Argument " << element << " is not valid" << std::endl;
+                return false;
+            }
+            town->name = tmp;
+            from = to + 1;
+            to = element.find(",", from);
+            tmp = element.substr(from, to);
+            if (tmp.find_first_not_of("0123456789") == std::string::npos) {
+                std::cout << "Argument " << element << " is not valid" << std::endl;
+                return false;
+            }
+            town->stamina = stoi(tmp);
+            from = to + 1;
+            tmp = element.substr(from);
+            if (tmp.find_first_not_of("0123456789") != std::string::npos) {
+                std::cout << "Argument " << element << " is not valid" << std::endl;
+                return false;
+            }
+            town->distance = stoi(tmp);
+            town->id = id;
+            towns.push_back(town);
+            id++;
+        }
+    }
+
+    return true;
+}
+
+void find_route(std::deque<t_town *> &towns) {
+    t_town *tmp = NULL;
+    int t = 0;
+    int energy = 0;
+
+    for (unsigned long int i = 0; i < towns.size(); i++) {
+        if (towns[t]->stamina < towns[t]->distance) {
+            tmp = towns.front();
+            towns.pop_front();
+            towns.push_back(tmp);
+            tmp = NULL;
+            t = 0;
+        }
+        else {
+            for (const auto town : towns) {
+                energy += town->stamina - town->distance;
+            }
+            if (energy < 0 && i < towns.size()) {
+                std::cout << "Mission: Impossible" << std::endl;
+                i = towns.size();
+            } else {
+                for (const auto& n : towns) {
+                    std::cout << n->id << " " << n->name << std::endl;
+                }
+                i = towns.size();
+            }
+        }
+    }
 }
 
 int main(int argc, char *argv[]) {
-    std::deque<t_town *> d_towns;
-    const int t = 5;
-    std::array<std::string, t> arr;
+    std::deque<t_town *> towns;
+    std::vector<std::string> arr;
 
     for (int i = 0; i < argc - 1; i++) {
-        arr[i] = argv[i];
+        arr.push_back(argv[i + 1]);
     }
-    // t_town *towns = NULL;
-    // std::vector<std::string> v_str;
-    // std::string str;
-    // std::string tmp;
-    // t_town town;
-    // int from = 0;
-    // int to = 0;
-
     if (argc == 1) {
         std::cout << "usage: ./visitAll [[name,stamina,distance] ...]" << std::endl;
-    } else {
-        // for (int i = 0; i < argc - 1; ++i) {
-        //     towns = new t_town;
-        //     str = argv[i + 1];
-        //     to = str.find(",");
-        //     // std::cout << to << "- to" << std::endl;
-        //     tmp = str.substr(0, to);
-        //     towns->name = tmp;
-        //     from = to + 1;
-        //     to = str.find(",", from);
-        //     tmp = str.substr(from, to);
-        //     towns->stamina = stoi(tmp);
-        //     from = to + 1;
-        //     tmp = str.substr(from);
-        //     towns->distance = stoi(tmp);
-        //     towns->id = i;
-        //     d_towns.push_back(towns);
-        //     str.clear();
-        // }
-        // for (int i = 0; i < d_towns.size(); ++i)
-        // {
-        // // int i = 1;
-        //     std::cout << "name " + d_towns[i]->name + " | stamina " << d_towns[i]->stamina << " | distance " << d_towns[i]->distance << std:: endl;
-        // }
+    } else if (check_input(towns, arr) == true) {
+        find_route(towns);
     }
-    // town.name = 
 
-    // d_towns.push_back();
     return 0;
 }
