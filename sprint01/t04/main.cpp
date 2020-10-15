f65b119c46ee9c16bbcd5190dacbed460f415e56
KozakMiksa <kozak.miksa@gmail.com>
2020-10-15 14:45:41 +0300

    t04 done

--- sprint01/t04/main.cpp
+++ sprint01/t04/main.cpp
@@ -1,35 +1,27 @@
 #include <iostream>
-#include <cmath>
 #include <string>
-#include <vector>
-#include <deque>
 #include <algorithm>
-#include <array>
-#include <list>
-#include <cstdlib>
-#include <ctime>
-#include <thread>
-#include <unistd.h>
 #include <fstream>
 #include <sstream>
 #include <forward_list>
-
-
 
 int main(int argc, char *argv[]) {
     std::ifstream fin;
     std::string str;
     std::forward_list<std::string> vstr;
     int list_size = 0;
-    // int str_size = 0;
-    // size_t rich = 0;
     bool r = 0;
     bool s = 0;
     int vel = 0;
-
+    int mel = 0;
+    
+    if (argc != 2) {
+        std::cout << "usage: ./stdAlgoV1 [file_name]" << std::endl;
+        exit(0);
+    }
     fin.open(argv[1]);
     if (!fin.is_open()) {
-    
+        std::cout << "error" << std::endl;
     } else {
         while (getline(fin, str, '\n')) {
             if (str.find("rich") == std::string::npos) {
@@ -41,24 +33,30 @@ int main(int argc, char *argv[]) {
             if (str.rfind("vel") == std::string::npos) {
                 vel++;
             }
+            if (str.find("mel") == std::string::npos) {
+                mel++;
+            }
             vstr.push_front(str);
             list_size++;
         }
+        std::cout << "size: " << list_size << std::endl;
+        std::cout << "contains 'rich': " << (r == 0 ? "false" : "true") << std::endl;
+        std::cout << "none of lengths is 15: " << (s == 0 ? "false" : "true") << std::endl;
+        std::cout << "all end with 'vel': " << (vel != list_size ? "false" : "true") << std::endl;
+        std::cout << "not contains 'mel': " << mel << std::endl;
     }
-    vstr.reverse();
-    // size = std::count(vstr.begin(), vstr.end(), );
-    std::cout << list_size << std::endl;
-    for (auto n : vstr) {
-        std::cout << n << std::endl;
-    }
-    // if (list_size == vel) {
-        std::cout << vel << std::endl;
-    // }
-    if (r == 1) {
-
-    }
-    std::cout << r << std::endl;
-    argc--;
 
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
