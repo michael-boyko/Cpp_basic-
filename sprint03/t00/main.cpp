def355abdbd4a62dff64f307295731bc93eb78c7
KozakMiksa <kozak.miksa@gmail.com>
2020-10-20 15:02:18 +0300

    t00 done

---
+++ sprint03/t00/main.cpp
@@ -0,0 +1,14 @@
+#include "templateAddition.h"
+#include <iostream>
+#include <string>
+
+int main() {
+    double d1 = 3;
+    double d2 = 4.3;
+
+    std::cout << add(d1, d2) << std::endl;
+    std::string s1 = "Hello";
+    std::string s2 = " there";
+    std::cout << add(s1, s2) << std::endl;
+    return 0;
+}
---
+++ sprint03/t00/templateAddition.h
@@ -0,0 +1,6 @@
+#pragma once
+
+template<typename T1, typename T2>
+    T2 add(T1 d1, T2 d2) {
+        return d1 + d2;
+    }
